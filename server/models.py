8346e05819383b3b4f69b5d434e9fa1bdb01c9ff
Vahan Nadjarian <vahan@nadjarian.com>
2023-07-05 12:01:17 -0400

    updated tables and readme

--- server/models.py
+++ server/models.py
@@ -1,5 +1,58 @@
+from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData, UniqueConstraint
 from sqlalchemy_serializer import SerializerMixin
-
+# from sqlalchemy.ext.hybrid import hybrid_property
+# from sqlalchemy.ext.associationproxy import association_proxy
+# from sqlalchemy.orm import validates
+# from flask_login import UserMixin
 from config import db
+import datetime
+
 
 # Models go here!
+
+# class User(db.Model, SerializerMixin):
+#     __tablename__ = 'users'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String, unique=True, nullable=False)
+#     name = db.Column(db.String)
+#     email = db.Column(db.String, unique=True, nullable=False)
+#     password = db.Column(db.String)
+
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    length = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    publication_date = db.Column(db.DateTime, nullable=False)
+
+class Award(db.Model, SerializerMixin):
+    __tablename__ = 'awards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+class BookAward(db.Model, SerializerMixin):
+    __tablename__ = 'book_awards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    # books = db.relationship
+    award_id = db.Column(db.Integer, db.ForeignKey('awards.id'))
+    # awards = db.relationship
+
+
+
