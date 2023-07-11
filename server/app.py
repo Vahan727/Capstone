2735dc246a5ec045492586e3671581040d95f938
Vahan Nadjarian <vahan@nadjarian.com>
2023-07-11 09:46:22 -0400

    database and routes

--- server/app.py
+++ server/app.py
@@ -9,15 +9,100 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # from models import User, Recipe
-from models import Author, Book
+from models import Author, Book, User, Library
 # Views go here!
 
 class Authors(Resource):
     def get(self):
-        authors = Author.query.all()
-        return authors
+        authors = [a.to_dict() for a in Author.query.all()]
+        return authors, 200
+    
+    def post(self):
+        data = request.get_json()
+        try:
+            new_author = Author(
+                name = data.get("name"),
+                date_of_birth = data.get("date_of_birth"),
+            )
+            db.session.add(new_author)
+            db.session.commit()
+            return new_author.to_dict(), 201
+        except Exception:
+            return ({"error": "400: Validation error"}, 400)
+
+class AuthorById(Resource):
+    def get(self, id):
+        try:
+            author = Author.query.filter(Author.id == id).first()
+            return author.to_dict(), 200
+        except:
+            return ({"error": "400: Validation error"}, 400)
+    
+    def patch(self, id):
+        data = request.get_json()
+        book = Book.query.filter(Book.id == id).first()
+        if not book:
+            return ({"error": "404 not found"}, 404)
+        for attr in data:
+            setattr(book, attr, data.get(attr))
+        db.session.add(book)
+        db.session.commit()
+        return book.to_dict(), 202
+
+class Books(Resource):
+    def get(self):
+        books = [b.to_dict() for b in Book.query.all()]
+        return books, 200
+    
+    def post(self):
+        data = request.get_json()
+        try:
+            new_book = Book(
+                title = data.get("title"),
+                length = data.get("length"),
+                publication_date = data.get("publication_date"),
+                author_id = data.get("author_id"),
+            )
+            db.session.add(new_book)
+            db.session.commit()
+            return new_book.to_dict(), 201
+        except:
+            return ({"error": "400: Validation error"}, 400)
+    
+class BookById(Resource):
+    def get(self, id):
+        try:
+            book = Book.query.filter(Book.id == id).first()
+            return book.to_dict(), 200
+        except:
+            return ({"error": "400: Validation error"}, 400)
+    
+    def patch(self, id):
+        data = request.get_json()
+        book = Book.query.filter(Book.id == id).first()
+        if not book:
+            return ({"error": "404 not found"}, 404)
+        for attr in data:
+            setattr(book, attr, data.get(attr))
+
+        db.session.add(book)
+        db.session.commit()
+        return book.to_dict(), 202
+    
+    def delete(self, id):
+        book = Book.query.filter(Book.id == id).first()
+        if not book:
+            return ({"error": "404 not found"}, 404)
+        db.session.delete(book)
+        db.session.commit()
+        return ({}, 204)
+    
+
 
 api.add_resource(Authors, "/authors")
+api.add_resource(AuthorById, "/authors/<int:id>")
+api.add_resource(Books, "/books")
+api.add_resource(BookById,"/books/<int:id>")
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -2,31 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 # from sqlalchemy import MetaData, UniqueConstraint
 from sqlalchemy_serializer import SerializerMixin
 # from sqlalchemy.ext.hybrid import hybrid_property
-# from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.associationproxy import association_proxy
 # from sqlalchemy.orm import validates
 # from flask_login import UserMixin
 from config import db
 import datetime
-
-
-# Models go here!
-
-# class User(db.Model, SerializerMixin):
-#     __tablename__ = 'users'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String, unique=True, nullable=False)
-#     name = db.Column(db.String)
-#     email = db.Column(db.String, unique=True, nullable=False)
-#     password = db.Column(db.String)
-
-
-class Author(db.Model, SerializerMixin):
-    __tablename__ = 'authors'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    age = db.Column(db.Integer, nullable=False)
 
 
 class Book(db.Model, SerializerMixin):
@@ -35,24 +15,89 @@ class Book(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     length = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    publication_date = db.Column(db.DateTime, nullable=False)
+    publication_date = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
 
-class Award(db.Model, SerializerMixin):
-    __tablename__ = 'awards'
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    author = db.relationship('Author', back_populates='books_by_author')
+
+    book_libraries = db.relationship("Library", back_populates="books")
+    user = association_proxy("book_libraries", "user")
+
+    serialize_only = (
+        "id", 
+        "title", 
+        "author_id",
+        "length",
+        "publication_date",
+        "image",
+    )
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String)
 
-class UserBooks(db.Model, SerializerMixin):
-    __tablename__ = 'user_books'
+    user_library = db.relationship("Library", back_populates="user")
+    books = association_proxy("user_library", "books")
+    
+    serialize_only = (
+        "id",
+        "username",
+        "name",
+        "email",
+        "password",
+    )
+
+class Library(db.Model, SerializerMixin):
+    __tablename__ = 'library'
 
     id = db.Column(db.Integer, primary_key=True)
     book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
-    # books = db.relationship
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    # awards = db.relationship
+    books = db.relationship("Book", back_populates="book_libraries")
 
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User", back_populates="user_library")
+
+    serialize_only = (
+        "id",
+        "book_id",
+        "user_id",
+    )
+
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    date_of_birth = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+
+    books_by_author = db.relationship('Book', back_populates="author")
+
+    serialize_only = (
+        "id",
+        "name",
+        "date_of_birth",
+        "image",
+    )
+
+# class Genre(db.Model, SerializerMixin):
+#     __tablename__ = 'genres'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+
+# class Award(db.Model, SerializerMixin):
+#     __tablename__ = 'awards'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
 
 
