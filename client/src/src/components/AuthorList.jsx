5328e452260aa41b08d17df5da184874c62bf6dd
Vahan Nadjarian <vahan@nadjarian.com>
2023-07-12 10:31:04 -0400

    updated components

--- client/src/src/components/AuthorList.jsx
+++ client/src/src/components/AuthorList.jsx
@@ -3,8 +3,6 @@ import AuthorCard from './AuthorCard'
 
 function AuthorList() {
     const [author, setAuthor] = useState([]);
-    // const [currentPage, setCurrentPage] = useState(1);
-    // const [totalPages, setTotalPages] = useState(0);
 
     useEffect(() => {
         fetchAuthors();
@@ -15,23 +13,10 @@ function AuthorList() {
         .then(response => response.json())
         .then(data => {
             setAuthor(data.authors);
-            // setTotalPages(data.pagination.pageTotal);
         })
         .catch(error => console.error(error));
     };
 
-    // const handleNextPage = () => {
-    //     setCurrentPage(prevPage => prevPage + 1);
-    // };
-
-    // const handlePreviousPage = () => {
-    //     setCurrentPage(prevPage => prevPage - 1);
-    // };
-
-    // const handleAddToFavorites = (bookId) => {
-    //     // Perform logic to add the player to the active squad
-    //     console.log('Adding book to user favorites:', bookId);
-    // };
 
     const mappedAuthors = authors.map((author) => {
         return (
@@ -49,32 +34,9 @@ function AuthorList() {
             search={search}
             handleSearch={handleSearch}
             />
-        {/* <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-            Previous Page
-        </button>
-        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-            Next Page
-        </button> */}
         {mappedAuthors}
         </main>
     )
-    //     <div>
-    //     <ul>
-    //         {books.map(book => (
-    //         <li key={book}>
-    //             {book}
-    //             <button onClick={() => handleAddToFavorites(book)}>Add to Favorites</button>
-    //         </li>
-    //         ))}
-    //     </ul>
-        // <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-        //     Previous Page
-        // </button>
-        // <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-        //     Next Page
-        // </button>
-        // </div>
-    // );
 }
 
 export default AuthorList;
--- client/src/src/pages/AuthorDetails.jsx
+++ client/src/src/pages/AuthorDetails.jsx
@@ -1,13 +1,56 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import BookCard from '../components/BookCard';
 
 
 function AuthorDetail() {
+
+const [author, setAuthor] = useState()
+
+const {id} = useParams()
+
+useEffect(() => {
+    getAuthor()
+}, [location])
+
+
+
+function getAuthor() {
+    fetch(`/api/authors/${id}`)
+    .then(resp => resp.json())
+    .then(data => setAuthor(data))
+}
+
+const mappedBooks = author.books_by_author.map((book) => {
     return (
-        <div>
-        <NavBar />
-        <h2>Author Details</h2>
-        </div>
+        <BookCard 
+            key={book.id}
+            author={book}
+            id={book.id}
+        />
+    )
+})   
+
+return ( 
+    <>
+    <div>
+    <NavBar />
+    <h2>Author Details</h2>
+    </div>
+    <div className="detailsContainer" >
+        <figure className="bookImg">
+            <img src={author.image} alt={author.name} />
+        </figure>
+        <p className="name">  {author.name}</p>
+        <p className="birth"> {author.date_of_birth} </p>
+    </div>
+    {mappedBooks}
+    <section>
+        <ul>
+            <button className="primary" >Add To Favorites</button> 
+        </ul>
+    </section>
+    </>
     );
 }
 
